574adb9f09bb16e3d52fa23d17e01f0f07e42458
Anthony <anthony.sforzin@gmail.com>
2025-04-17 22:12:07 -0300

    feat(hero): adiciona hero com título animado via fade-in

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const titulo = document.getElementById('titulo');
+  
+    setTimeout(() => {
+      titulo.classList.add('fade-in');
+    }, 200);
+  });
+  
